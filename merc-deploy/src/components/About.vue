381823475492905f4374a046b189ab71956a7f7f
Mike Mercuro <mmercuro21@gmail.com>
2021-02-27 18:18:55 -0500

    added vuetify

---
+++ merc-deploy/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title></title>
+  </head>
+  <body>
+  <h1>A little about me</h1>
+  </body>
+  </html>
+</template>
+
+<script>
+  export default {
+    name: 'About',
+
+  }
+</script>
